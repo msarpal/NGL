6d7ae9ea611b486cd3e7eacf1f2e62bdc651e7c5
SARPAL Mohit <mohit.sarpal@soprasteria.com>
2018-02-23 16:27:48 +0530

    HTML parsing done

--- src/Parser/HtmlParser.js
+++ src/Parser/HtmlParser.js
@@ -1,6 +1,7 @@
 var request = require("request");
 var cheerio = require("cheerio");
 var fs = require("fs");
+var JSSoup = require('jssoup').default;
 
 var express = require('express');
 var router = express.Router();
@@ -10,29 +11,42 @@ module.exports = router;
 
 var data = [];
 var url = "";
+var soup = "";
 
 function htmlParse(req, res) {
-    //http://localhost:4000/api/parse/form?url=https://www.indeed.com/jobs?l=Los+Angeles&_ga=2.200827310.1261035140.1519200447-880084272.1519200447
+    //http://localhost:4000/api/parse/form?url=https://unsplash.com/login
     if (req.query.url != undefined) {
         url = req.query.url;
         console.log("started!\n ");
         request(url, function (error, response, html) {
             if (!error && response.statusCode == 200) {
-                var $ = cheerio.load(html);
+                // var $ = cheerio.load(html);
 
-                $('input').filter(function () {
-                    var output = $(this).prev();
-                     console.log(output.text());
+                // $('form').find('label').each(function (i,e) {
+                //     var output = $(this);
+                //     // console.log(output.text());
 
                     
-                    if (output.text() != "") {
-                        data.push(output.text().replace(/[^a-zA-Z ]/g, "").replace(/^\s+|\s+$/g, ''));
-                    }
-                    // console.log(output.text());
+                //      if (output.text() != "") {
+                         
+                //          data.push(output.text().replace(/[^a-zA-Z ]/g, "").replace(/^\s+|\s+$/g, ''));
+                //      }
+                   
 
-                });
-                console.log(data);
+                // });
+                // console.log(data);
+
+                soup = new JSSoup(html); 
+               
+                var listform = ["text","radio", "checkbox", "password", "file", "image",/*"hidden"*/];
+                var result = soup.findAll('input', {'type': listform});
+                console.log('res '+result);
+                for(var index = 0 ; index < result.length ; index++){
+                    console.log(result[index].attrs.id+"");
+                }
+                
             }
+          
         });
 
     }
